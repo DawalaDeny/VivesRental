2678fbc9c61be58ebb16dfc2d2b6496ea14f145f
shabo <shabo@DESKTOP-4VN525A>
2023-05-20 17:05:40 +0200

    index

--- Vives/Controllers/HomeController.cs
+++ Vives/Controllers/HomeController.cs
@@ -1,20 +1,45 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Vives.Models;
+using Vives.SDK;
+using VivesRental.UI.Models;
+using VivesRental.UI.SDK;
 
 namespace Vives.Controllers
 {
 	public class HomeController : Controller
 	{
-		
-		public HomeController()
-		{
-			
-		}
+        private readonly OrderSdk ordSdk;
+        private readonly CustomerManagementSdk cusSdk;
+        private readonly ProductManagementSdk proSdk;
+        private readonly ArticleSdk artSdk;
+        private readonly ReservationSdk resSdk;
 
-		public IActionResult Index()
-		{
-			return View();
+        public HomeController(OrderSdk ordSdk, CustomerManagementSdk cusSdk, ProductManagementSdk proSdk, ArticleSdk artSdk, ReservationSdk resSdk)
+        {
+			this.ordSdk = ordSdk;
+            this.cusSdk = cusSdk;
+			this.proSdk = proSdk;
+			this.artSdk = artSdk;
+			this.resSdk = resSdk;
+        }
+
+		public async Task<IActionResult> Index()
+        {
+            var listCustomers = await cusSdk.FindAsync();
+			var listProducts = await proSdk.FindAsync();
+			var listArticles = await artSdk.FindAsync();
+			var listOrders = await ordSdk.FindAsync();
+			var listReservations = await resSdk.FindAsync();
+
+            var alles = new IndexPaginaDetails();
+            alles.Customers = listCustomers;
+            alles.Articles = listArticles;
+			alles.Orders = listOrders;
+			alles.Reservations = listReservations;
+            alles.Products = listProducts;
+			
+            return View(alles);
 		}
 
 
---
+++ Vives/Models/IndexPaginaDetails.cs
@@ -0,0 +1,16 @@
+﻿using Vives.SDK;
+using VivesRental.Services.Model.Results;
+using VivesRental.UI.SDK;
+
+namespace VivesRental.UI.Models
+{
+    public class IndexPaginaDetails
+    {
+        public IList<CustomerResult> Customers { get; set; }
+        public IList<ProductResult> Products { get; set; }
+        public IList<ArticleResult> Articles { get; set; }
+        public IList<OrderResult> Orders { get; set; }
+        public IList<ArticleReservationResult> Reservations { get; set; }
+
+    }
+}
