20b34a3e1a4c1c0ab23bc272b2345ce585d6a997
shabo <shabo@DESKTOP-4VN525A>
2023-05-21 21:08:55 +0200

    form validatie

--- VivesRental.Services.Model/Requests/CustomerRequest.cs
+++ VivesRental.Services.Model/Requests/CustomerRequest.cs
@@ -4,8 +4,12 @@ namespace VivesRental.Services.Model.Requests;
 
 public class CustomerRequest
 {
+    [StringLength(50, ErrorMessage = "Name length can't be more than 50.")]
     [Required] public string FirstName { get; set; } = null!;
+    [StringLength(50, ErrorMessage = "Name length can't be more than 50.")]
     [Required] public string LastName { get; set; } = null!;
+
     [Required][EmailAddress] public string Email { get; set; } = null!;
+    [Range(0, long.MaxValue, ErrorMessage = "Please enter a realistic phone number.")]
     [Required] public string PhoneNumber { get; set; } = null!;
 }
--- VivesRental.Services.Model/Requests/ProductRequest.cs
+++ VivesRental.Services.Model/Requests/ProductRequest.cs
@@ -4,9 +4,14 @@ namespace VivesRental.Services.Model.Requests;
 
 public class ProductRequest
 {
+    [StringLength(50, ErrorMessage = "Name length can't be more than 50.")]
     [Required] public string Name { get; set; } = null!;
+    [StringLength(50,ErrorMessage = "Name length can't be more than 50.")]
     public string? Description { get; set; }
+    [StringLength(50, ErrorMessage = "Name length can't be more than 50.")]
     public string? Manufacturer { get; set; }
+    [StringLength(50, ErrorMessage = "Name length can't be more than 50.")]
     public string? Publisher { get; set; }
+    [Range(0, 100, ErrorMessage = "Please enter number.")]
     public int RentalExpiresAfterDays { get; set; }
 }
